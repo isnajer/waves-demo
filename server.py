742a7bb3f7fcdff4445c9223b7a8483ec3f1edbc
Isis Najera <isisnajer@gmail.com>
2022-10-29 19:05:18 -0500

    3-Nav Bars

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from model import connect_to_db, db, User
 from sqlalchemy.exc import IntegrityError
 import bcrypt
 import os
+import re
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -24,53 +25,20 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route("/dashboard")
-def user_details():
-    """Users dashboard."""
-
-    user_id = session.get("user_id")
-
-    # Is user logged in?
-    if user_id:
-        user = User.query.get(user_id)
-        return render_template("dashboard.html", user=user)
-
-    # If not, redirect to homepage.
-    else:
-        flash("You are not logged in.")
-        return redirect('/')
-
-
-@app.route("/login", methods=["POST"])
-def user_login():
-    """Logs user in"""
-
-    email = request.form.get("email") #from HTML
-    password = request.form.get("password")
-
-    user = crud.get_user_by_email(email) #<- invoking function from crud.py
-    if not user or user.password != password:
-        return render_template("dashboard.html", user=user)
-    else:
-        session['user_email'] = user.email #getting email from object / but has same value as email line 48 (matching)
-        # ^ dictionary of user_email: email (as value)
-        flash("Logged in succesfully!")
-
-    #if user does not exist then flash message error
-    #else user exists check if password is correct
-        return redirect("/dashboard.html")
-
-
 @app.route("/users", methods=["POST"])
 def register_user():
     """Create a new user"""
 
     email = request.form.get("email")
+    
     password = request.form.get("password")
-
     user = crud.get_user_by_email(email)
-    if user: #if user exists 
-        flash("Cannot create an account with that email. Try again.")
+    match_obj = re.search(r"(\w+)\@(\w+\.com)", email)
+    if match_obj is None:
+        flash("Invalid email address")
+        return redirect('/')
+    elif user: #if user exists 
+        flash("User with this email already exists.")
     else:
         user = crud.create_user(email, password)
         db.session.add(user)
@@ -79,63 +47,48 @@ def register_user():
     
     return redirect("/")
 
+### ASK IF I STILL NEED CODE IN THIS:
+# @app.route("/dashboard")
+# def user_details():
+#     """Users dashboard."""
+#     # return "THIS IS THE DASHBOARD!"
+#     user_id = session.get("user_id")
+
+#     # Is user logged in?
+#     if user_id:
+#         user = User.query.get(user_id)
+#         return render_template("dashboard.html", user=user)
+
+#     # If not, redirect to homepage.
+#     else:
+#         flash("You are not logged in.")
+#         return redirect('/')
+
+
+@app.route("/dashboard", methods=["POST"])
+def login():
+    """Logs user in / Directs them to user details dashboard if login successful."""
+
+    email = request.form.get("email") #from HTML
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email) #<- invoking function from crud.py
+    if not user:
+        flash("User email does not exist.")
+        return redirect("/")
+    elif user.password != password:
+        flash("Password is incorrect!")
+        return redirect("/")
+    else:
+        session['user_email'] = user.email #getting email from object / but has same value as email line 48 (matching)
+        # ^ dictionary of user_email: email (as value)
+        return render_template("/dashboard.html")
+    #if user does not exist then flash message error
+    #else user exists check if password is correct
+        
+
+
 
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
-
-
-
-
-### CHANGE TO THESE WHEN EVERYTHING WORKS! ###
-
-# @app.route('/login', methods=['POST'])
-# def login():
-#     """Check if user exists. Redirect to dashboard."""
-
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-
-#     # user = crud.get_user_by_email(email)
-#     user = User.query.filter(User.email==email).first()
-#     if not user:
-#         flash("User does not exist.")
-#         return redirect("/")
-
-#     if user.password != bcrypt.hashpw(password.encode(), user.password):
-#         flash("Incorrect password")
-#         return redirect("/")
-
-#     session["user_id"] = user.user_id
-
-#     flash("Logged in sucesfully!")
-#     return redirect("/dashboard")
-
-
-# @app.route('/sign_up', methods= ['POST'])
-# def sign_up():
-#     """Add new user into database."""
-
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-#     name = request.form.get('name', '').strip()
-#     if not name:
-#         flash("Must enter a name!")
-#         return redirect('/')
-
-#     salt = bcrypt.gensalt()
-#     hashed_password = bcrypt.hashpw(password.encode(), salt)
-#     try:
-#         new_user = User(email=email, password=hashed_password, name=name)
-#         db.session.add(new_user)
-#         db.session.commit()
-#     except IntegrityError:
-#         flash('User with this email already exists')
-#         return redirect('/')
-
-#     # Add new user to the session
-#     new_user = User.query.filter(User.email==email).one()
-#     session['user_id'] = new_user.user_id
-
-#     flash("User {} added.".format(name))
-#     return redirect("/dashboard")
