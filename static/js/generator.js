5684f6065a932e00fc004f925c3d7ea1019a5cf6
Isis Najera <isisnajer@gmail.com>
2022-11-14 14:06:05 -0800

    Quote Gen. working :)

--- static/js/generator.js
+++ static/js/generator.js
@@ -1,6 +1,6 @@
 // function that randomly selects quote from dict.
 
-let btn = document.getElementById('btn');
+let quote_gen = document.getElementById('quote_gen');
 let output = document.getElementById('output');
 let quotes = 
     [' "If you want to find the secrets of the universe, think in terms of energy, frequency and vibration." - Nikola Tesla',
@@ -9,27 +9,7 @@ let quotes =
     ];
 
 
-btn.addEventListener('click', function(){
-    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+window.onload =  function(){
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
     output.innerHTML = randomQuote;
-})
-
-// what I think it should be like:
-
-// document.querySelector('#output').addEventListener('load', (evt) => {
-//     evt.preventDefault();
-    
-//     let output = document.getElementById('output');
-//     let quotes = 
-//         [' "If you want to find the secrets of the universe, think in terms of energy, frequency and vibration." - Nikola Tesla',
-//          ' "Sound is the Medicine of the future." - Edgar Cayce',
-//          ' "If you do not make time for your wellness, you will be forced to make time for your illness." - N/A',
-//         ];
-    
-    
-//     output.addEventListener('load', function(){
-//         var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-//         output.innerHTML = randomQuote;
-//     })
-    
-//     });
+};
